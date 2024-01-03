fd7479916247ec5aaae4656175897b598866aac0
Andrey Budovsky <uyean@yandex.ru>
2024-01-04 02:16:39 +0300

    refactor(extended-3.1 - 3.8): update html, css and js - add BEM, meaningful naming, names of identifiers and classes concise, changed the tag selector to classes, add indentation in blocks, add space after property name, add semicolon after properties and add separate selectors and properties

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("newTask");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var completedTasksHolder=document.getElementById("completedTasks");//completed-tasks
 
 
 //New task list item
@@ -32,23 +32,27 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="todo-app__item";
+    console.log(taskString);
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='todo-app__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="todo-app__checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.value=taskString;
+    editInput.className="todo-app__task input-reset";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
-
-    deleteButton.className="delete";
+    editButton.className="todo-app__edit-btn btn-reset";
+    deleteButton.className="todo-app__delete-btn btn-reset";
     deleteButtonImg.src='./remove.svg';
-    deleteButton.appendChild(deleteButtonImg);
-
+    deleteButtonImg.className='todo-app__icon';
 
     //and appending.
+    deleteButton.appendChild(deleteButtonImg);
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
     listItem.appendChild(editInput);
@@ -79,27 +83,25 @@ var editTask=function(){
     console.log("Edit Task...");
     console.log("Change 'edit' to 'save'");
 
-
     var listItem=this.parentNode;
+    var editInput=listItem.querySelector('.todo-app__task');
+    var label = listItem.querySelector(".todo-app__label");
+    var editBtn=listItem.querySelector(".todo-app__edit-btn");
+    var containsClass=listItem.classList.contains("todo-app__item--edit");
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
-
         //switch to .editmode
         //label becomes the inputs value.
-        label.innerText=editInput.value;
+        label.textContent = editInput.value;
         editBtn.innerText="Edit";
-    }else{
-        editInput.value=label.innerText;
+    } else {
+        editInput.value = label.textContent;
         editBtn.innerText="Save";
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("todo-app__item--edit");
 };
 
 
@@ -155,9 +157,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var checkBox=taskListItem.querySelector(".todo-app__checkbox");
+    var editButton=taskListItem.querySelector(".todo-app__edit-btn");
+    var deleteButton=taskListItem.querySelector(".todo-app__delete-btn");
 
 
     //Bind editTask to edit button.
